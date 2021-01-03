177b1f21f41a364812bafa99e48f811c1898025b
Vladimir Torres Muñoz <v.torres.titanes@gmail.com>
2021-01-03 19:22:44 -0300

    Titan región y comuna de Chile
    Plugin que permite agregar al checkout de woocommerce región y comunas Chilenas.

---
+++ plugin-modulo-region-comuna/includes/js/titan_comuna.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+
+
+    $(function() {
+        $('#billing_city_field, #billing_state_field').wrapAll('<div class="modulo-regiones-titan"></div>');
+
+    });
+
+});
